cd8f62b64624adceadabc463fea6065ee64a535c
Mohamed Fathy <mofasa.peace@gmail.com>
2022-03-18 17:07:56 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class System:
+    def __init__(self,DOF,DH_matrix):
+        self.DOF = DOF
+        self.DH_matrix = DH_matrix
+
+    def forward_kinamatics():
+        pass
+
+    def inverse_kinamatics():
+        pass
+
+    def jacobian():
+        pass
+
+    def trajectory():
+        pass
+
