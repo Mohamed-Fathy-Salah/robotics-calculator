d5d40f6f2973ce0a6420137f799851134b5bbc7b
Mohamed Fathy <mofasa.peace@gmail.com>
2022-03-22 17:38:55 +0200

    parameters & helper classes

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+from dataclasses import dataclass
+from enum import Enum
 import numpy as np
 
 class System:
-    def __init__(self,DOF,DH_matrix):
-        self.DOF = DOF
-        self.DH_matrix = DH_matrix
+    # joints    : list of Joint objects
+    # DH_matrix : list of DH objects
+    # A         : list of matrices contains A1, A2, ..., An
+    # T         : list of matrices contains T01, T02, ...,T0n
+    def __init__(self, joints, DH_list):
+        self.joints = joints
+        self.A = [i.get_A_matrix() for i in DH_list]
+        self.T = []
 
-    def forward_kinamatics():
+    # TODO : add other constants also ( add it from the begining in DH)
+    def forward_kinamatics(joint_variables):
+        # compensate in T0n with joint_variables & return x, y, z, phi, theta, psi
         pass
-
+        
     def inverse_kinamatics():
         pass
 
@@ -16,4 +25,19 @@ class System:
 
     def trajectory():
         pass
+
+
+@dataclass
+class DH:
+    a: str
+    alpha: str
+    d: str
+    theta: str
+    def get_A_matrix():
+        # TODO : generate A matrix for this joint
+        pass
+
+class Joint(Enum):
+    PRISMATIC = 1
+    REVOLUTE = 2
 
