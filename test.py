56917a59c53916fc5ca1f168023a28d257cdddeb
Mohamed Fathy <mofasa.peace@gmail.com>
2022-03-28 19:42:10 +0200

    refined test.py

--- test.py
+++ test.py
@@ -1,20 +1,32 @@
 from factory import *
 
+print(">> DH parameters <<")
+print("a\talpha\td\ttheta")
+print()
+print("0\t0\t1\tt1")
+print("0\t-pi/2\td2\t0")
+print("0\t0\td3\t0")
+
 sys = Factory(0)
 print("-------------- forward kinamatics --------------")
 sys.fk()
-input()
+input("press enter to continue")
+
 print("-------------- inverse kinamatics --------------")
 sys.ik()
-input()
+input("press enter to continue")
+
 print("-------------- jacobian --------------")
 sys.jacob()
-input()
+input("press enter to continue")
+
 print("-------------- T_new --------------")
 sys.j()
-input()
+input("press enter to continue")
+
 print("-------------- inverse jacobian --------------")
 sys.inv_jacob()
-input()
+input("press enter to continue")
+
 print("-------------- trajectory --------------")
 sys.t()
